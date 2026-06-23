c6691677f4837e2b7e284edadb2d545f0b2cb067
alexikeda-ifsp <alex.ikeda@aluno.ifsp.edu.br>
2026-06-23 16:57:20 -0300

    Ajustes no connection_api

--- paciente-api/connection_api.py
+++ paciente-api/connection_api.py
@@ -1,27 +1,22 @@
 import os
 import psycopg2
+
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
 
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = os.getenv("DB_PORT")
-
-
 def create_connection():
+
     try:
 
         connection = psycopg2.connect(
-            host=DB_HOST,
-            database=DB_NAME,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            port=DB_PORT
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
         )
 
         return connection
@@ -29,6 +24,6 @@ def create_connection():
 
     except Exception as e:
 
-        print(f"Erro na conexão com PostgreSQL: {e}")
+        print(f"Erro ao conectar no PostgreSQL: {e}")
 
         return None
